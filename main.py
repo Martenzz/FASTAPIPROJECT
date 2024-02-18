fd695bd73a52e5172af19a490c9c3f7342279b40
zakari martins <zakarimartins@gmail.com>
2024-02-18 22:41:25 +0100

    changes to local host address

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ def delete_student(student_id: int):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
